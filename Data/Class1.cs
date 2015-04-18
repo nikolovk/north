df312126cde992784c69b752af260aa40af6054c
Krasi Nikolov <krasi.nikolov@gmail.com>
2015-04-18 14:52:59 +0300

    add database first EF

---
+++ Data/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Data.Context;
+
+namespace Data
+{
+    public class Class1
+    {
+        NorthwindEntities dbContext = new NorthwindEntities();
+    }
+}
